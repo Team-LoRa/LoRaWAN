e0db01cb92e74e5bdce3742d1076b3ca343dbed1
Benjamin Couey <bhc36@nau.edu>
2020-04-27 17:44:55 -0600

    Split sendLoRaMessage() into encoding and forwarding functions. Added message headers.

--- LoraWANApplication/loralibrary/src/main/java/com/teamlora/loralibrary/LoraNodeConnection.kt
+++ LoraWANApplication/loralibrary/src/main/java/com/teamlora/loralibrary/LoraNodeConnection.kt
@@ -22,15 +22,41 @@ class LoRaMessenger( val appName: String, jsonString: String ) {
 
     private var encodingTable : JSONObject? = null
 
-    private var encodedMessage : ByteArray = ByteArray( 0 )
-
     init {
         // Convert the passed string to a JSONObject to access as the encoding table
         encodingTable = JSONObject( jsonString )
     }
 
+    // The main wrapper method that is called by the developer to encode and send a message over
+    // LoRaWAN.
+    // Parameters
+    // apiName - The name of the API this message is for, as it appears in the encoding_table.json
+    // parameters - An array of parameters to be included with the message. Presently
+    //              unsigned integers, floating point numbers, and doubles are supported data types
     fun sendLoRaMessage(apiName: String, parameters: Array<Any>)
     {
+        val Thread1: Thread = Thread()
+        {
+            // Encode the message
+            val encodedMessage = encodeMessage( apiName, parameters )
+
+            // Send off the encoded message
+            forwardMessage( encodedMessage )
+        }
+        Thread1.start()
+    }
+
+    // The method responsible for running the decoding subprocess. It iterates through the items
+    // in the parameters array and, for each item, converts it into a byte representation as per the
+    // definition found in encoding_table.json.
+    // Parameters
+    // apiName - The name of the API this message is for, as it appears in the encoding_table.json
+    // parameters - An array of parameters to be included with the message. Presently
+    //              unsigned integers, floating point numbers, and doubles are supported data types
+    fun encodeMessage(apiName: String, parameters: Array<Any>): ByteArray {
+
+        var encodedMessage : ByteArray = ByteArray( 0 )
+
         Log.d("myTag", "sendLoRaMessage started" )
         // Access the app and api tables
         // Use !! to assert that encodingTable is not null
@@ -179,94 +205,100 @@ class LoRaMessenger( val appName: String, jsonString: String ) {
             Log.d("myTag", byte.toUByte().toString() )
         }
 
-        // Send off the encoded message
-        val Thread1: Thread = Thread()
-        {
-            // Determine how many payloads will need to be sent
-            val payloadTotal = ceil( encodedMessage.size.toDouble() / PAYLOAD_BYTE_COUNT.toDouble() ).toInt()
+        return encodedMessage
+    }
 
-            Log.d("myTag", "the payload count is: ")
-            Log.d("myTag", payloadTotal.toString() )
+    // The method responsible for fragmenting and forwarding the given message. It randomly determines
+    // an 2 byte ID, the total number of packets the message will take, and includes this information
+    // as metadata at the head of up to 10 bytes of the message. These packets are preceded by a 4 byte
+    // header that defines the total length of the fragmented message with metadata.
+    // The IP address to which the library forwards the message is hard-coded in this function.
+    // Parameters
+    // message - The encoded message to be forwarded to the static IP
+    fun forwardMessage( message: ByteArray ) {
+        var encodedMessage = message
 
-            // Determine the metadata that will be prepended to each payload
-            var messageID : ByteArray = ByteArray( 2 )
+        // Determine how many payloads will need to be sent
+        val payloadTotal = ceil( encodedMessage.size.toDouble() / PAYLOAD_BYTE_COUNT.toDouble() ).toInt()
 
-            messageID = nextBytes( messageID )
+        Log.d("myTag", "the payload count is: ")
+        Log.d("myTag", payloadTotal.toString() )
 
-            //Assign the Ip Address 192.168.1.101
-            val buffer = ByteArray(4)
+        // Determine the metadata that will be prepended to each payload
+        var messageID : ByteArray = ByteArray( 2 )
 
-            buffer.set(0, 192.toByte())
-            buffer.set(1, 168.toByte())
-            buffer.set(2, 0.toByte())
-            buffer.set(3, 46.toByte())
-            Log.d("myTag", "the url host is: ")
+        messageID = nextBytes( messageID )
 
-            val ipAddress = InetAddress.getByAddress(buffer)
-            Log.d("myTag", "the ip address is: " + ipAddress)
+        //Assign the Ip Address 192.168.1.101
+        val buffer = ByteArray(4)
 
-            val clientSocket: Socket = Socket(ipAddress, 2080)
+        buffer.set(0, 192.toByte())
+        buffer.set(1, 168.toByte())
+        buffer.set(2, 0.toByte())
+        buffer.set(3, 46.toByte())
+        Log.d("myTag", "the url host is: ")
 
-            // Calculate the total number of bytes the message will take up and send that as a four byte head
-            val totalLength = encodedMessage.size + ( payloadTotal * 3 )
+        val ipAddress = InetAddress.getByAddress(buffer)
+        Log.d("myTag", "the ip address is: " + ipAddress)
 
-            // Start with an empty byte array
-            val mask : Int = 0xFF // binary 1111 1111
-            var lengthByteArray : ByteArray = ByteArray( 0 )
+        val clientSocket: Socket = Socket(ipAddress, 2080)
+
+        // Calculate the total number of bytes the message will take up and send that as a four byte header
+        val totalLength = encodedMessage.size + ( payloadTotal * 3 )
+
+        // Start with an empty byte array
+        val mask : Int = 0xFF // binary 1111 1111
+        var lengthByteArray : ByteArray = ByteArray( 0 )
 
 
-            // Take the interger, 8 bits at a time, and append it to the byte array
-            var _parameter : Int = totalLength
-            for( i in 0 until 4 ) {
-                lengthByteArray +=_parameter.and( mask ).toByte()
-                _parameter = _parameter.shr( 8 )
-            }
+        // Take the interger, 8 bits at a time, and append it to the byte array
+        var _parameter : Int = totalLength
+        for( i in 0 until 4 ) {
+            lengthByteArray +=_parameter.and( mask ).toByte()
+            _parameter = _parameter.shr( 8 )
+        }
 
-            // Reverse the byte array to maintain big endian order
-            lengthByteArray.reverse()
+        // Reverse the byte array to maintain big endian order
+        lengthByteArray.reverse()
 
-            Log.d("myTag", "Header sent:" )
-            for( byte in lengthByteArray ) {
+        Log.d("myTag", "Header sent:" )
+        for( byte in lengthByteArray ) {
+            Log.d("myTag", byte.toUByte().toString() )
+        }
+
+        // Send this header
+        clientSocket.outputStream.write(lengthByteArray)
+
+        // Send some number of payloads
+        for( i in 0 until payloadTotal ) {
+            // Send a packet of the form
+            // messageID : payloadNumber/payloadTotal : 10 bytes of encodedMessage
+            var packetArray : ByteArray = ByteArray( 0 )
+
+            // Combined the payload number and payload total into a single byte
+            val payloadNumber = i.shl( 4 )
+            val combinedPayloadByte = payloadNumber + payloadTotal
+
+            // Add all of the metadata
+            packetArray += messageID
+            packetArray += combinedPayloadByte.toByte()
+
+            // Add up to PAYLOAD_BYTE_COUNT bytes from the total message to the packet
+            packetArray += encodedMessage.slice( IntRange( 0, min( PAYLOAD_BYTE_COUNT, encodedMessage.size ) - 1 ) )
+
+            Log.d("myTag", "Packet sent:" )
+            for( byte in packetArray ) {
                 Log.d("myTag", byte.toUByte().toString() )
             }
 
-            // Send this header
-            clientSocket.outputStream.write(lengthByteArray)
+            // Actually send the packet
+            clientSocket.outputStream.write(packetArray)
 
-            // Send some number of payloads
-            for( i in 0 until payloadTotal ) {
-                // Send a packet of the form
-                // messageID : payloadNumber/payloadTotal : 10 bytes of encodedMessage
-                var packetArray : ByteArray = ByteArray( 0 )
-
-                // Combined the payload number and payload total into a single byte
-                val payloadNumber = i.shl( 4 )
-                val combinedPayloadByte = payloadNumber + payloadTotal
-
-                // Add all of the metadata
-                packetArray += messageID
-                packetArray += combinedPayloadByte.toByte()
-
-                // Add up to PAYLOAD_BYTE_COUNT bytes from the total message to the packet
-                packetArray += encodedMessage.slice( IntRange( 0, min( PAYLOAD_BYTE_COUNT, encodedMessage.size ) - 1 ) )
-
-                Log.d("myTag", "Packet sent:" )
-                for( byte in packetArray ) {
-                    Log.d("myTag", byte.toUByte().toString() )
-                }
-
-                // Actually send the packet
-                clientSocket.outputStream.write(packetArray)
-
-                encodedMessage = encodedMessage.drop( min( PAYLOAD_BYTE_COUNT, encodedMessage.size ) ).toByteArray()
-            }
-
-            // Close the socket
-            clientSocket.close()
+            encodedMessage = encodedMessage.drop( min( PAYLOAD_BYTE_COUNT, encodedMessage.size ) ).toByteArray()
         }
-        Thread1.start()
+
+        // Close the socket
+        clientSocket.close()
     }
 
 }
-
-
